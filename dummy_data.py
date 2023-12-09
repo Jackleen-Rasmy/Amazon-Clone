2b4ebafe6d521532eaa24dc8795900e78fedf79c
Jackleen Rasmy <jackleen.rasmy@gmail.com>
2023-12-09 17:37:31 +0200

    add dummy data

---
+++ dummy_data.py
@@ -0,0 +1,40 @@
+import os, django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+django.setup()
+
+from faker import Faker 
+from products.models import Brand , Product, Review
+import random
+
+
+def brand_seed(n):
+    fake = Faker()
+    images = ['01.jpg','02.jpg','03.jpg','04.jpg','05.jpg','06.jpg','07.jpg']
+    
+    for _ in range(n):
+        Brand.objects.create(
+            name = fake.name(),
+            image = f"brand/{images[random.randint(0,6)]}"
+        )
+        
+def product_seed(n):
+    fake = Faker()
+    flag_types = ['New','Sale','Feature']
+    images = ['01.jpg','02.jpg','03.jpg','04.jpg','05.jpg','06.jpg','07.jpg','08.jpg']
+    brands = Brand.objects.all()
+    
+    for _ in range(n):
+        Product.objects.create(
+            name = fake.name(),
+            flag = flag_types[random.randint(0,2)],
+            price = round(random.uniform(20.99,99.99),2),
+            image = f"product/{images[random.randint(0,7)]}",
+            sku = random.randint(100,1000000),
+            subtitle = fake.text(max_nb_chars=450),
+            description = fake.text(max_nb_chars=4000),
+            brand = brands[random.randint(0,len(brands)-1)]
+            
+        )
+
+# brand_seed(200)
+product_seed(1000)
