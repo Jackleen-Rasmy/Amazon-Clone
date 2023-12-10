a811e18c4bb23e4b50ae391fd78b47cfa4172c55
Jackleen Rasmy <jackleen.rasmy@gmail.com>
2023-12-10 11:06:44 +0200

    add images to dummy data

--- dummy_data.py
+++ dummy_data.py
@@ -9,18 +9,18 @@ import random
 
 def brand_seed(n):
     fake = Faker()
-    images = ['01.jpg','02.jpg','03.jpg','04.jpg','05.jpg','06.jpg','07.jpg']
+    images = ['01.jpg','02.jpg','03.jpg','04.jpg','05.jpg','06.jpg','07.jpg','08.jpg','09.jpg','10.jpg']
     
     for _ in range(n):
         Brand.objects.create(
             name = fake.name(),
-            image = f"brand/{images[random.randint(0,6)]}"
+            image = f"brand/{images[random.randint(0,9)]}"
         )
         
 def product_seed(n):
     fake = Faker()
     flag_types = ['New','Sale','Feature']
-    images = ['01.jpg','02.jpg','03.jpg','04.jpg','05.jpg','06.jpg','07.jpg','08.jpg']
+    images = ['01.jpg','02.jpg','03.jpg','04.jpg','05.jpg','06.jpg','07.jpg','08.jpg','09.jpg','10.jpg']
     brands = Brand.objects.all()
     
     for _ in range(n):
@@ -28,7 +28,7 @@ def product_seed(n):
             name = fake.name(),
             flag = flag_types[random.randint(0,2)],
             price = round(random.uniform(20.99,99.99),2),
-            image = f"product/{images[random.randint(0,7)]}",
+            image = f"product/{images[random.randint(0,9)]}",
             sku = random.randint(100,1000000),
             subtitle = fake.text(max_nb_chars=450),
             description = fake.text(max_nb_chars=4000),
